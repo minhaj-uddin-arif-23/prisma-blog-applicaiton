b500dea889aa6cc39dd4b6e2d84016c974fd8dbf
minhajuddinarif <arifk62901@gmail.com>
2025-12-30 12:06:37 +0600

    setup better-auth to authentication and save it database

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express, { type Application } from "express";
 import { postRouter } from "./modules/post/post.route";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use(express.json());
-
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/post", postRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! Hi there.");
+  res.send("Hello, World! Hi there this is a good day.");
 });
 
 export default app;
